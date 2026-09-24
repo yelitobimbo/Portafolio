efaf97632bda609b6d5cb223050ac5183b90e509
yelitobimbo <1225100375.ysc@gmail.com>
2026-09-24 15:58:34 -0600

    Saludo de mi nombre

--- datos/unidad1/recursividad/Recursividad.java
+++ datos/unidad1/recursividad/Recursividad.java
@@ -1,15 +1,16 @@
 package  datos.unidad1.recursividad;
 public class Recursividad {
-	public static  void saludo(int total) {
-	if(total <= 0) // condicion Base
-	return;
 
-	else { // Condicion recursiva o repetitiva
-	System.out.println("Hola");
-	saludo(total-1);
-	}
-}
-	public static void main(String[ ] args ){
-	saludo(10);
-	}
+    public static void saludo(int total, String nombre) {
+        if (total <= 0) //Condicion base
+  	return;
+           else{ //Condicion recursiva o repetitiva
+	    System.out.println("Hola  " + nombre);
+            saludo(total - 1, nombre);
+        }
+    }
+
+    public static void main(String[ ] args) {
+        saludo(10, "Yaneli");
+    }
 }
