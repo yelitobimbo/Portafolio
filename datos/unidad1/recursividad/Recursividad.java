39ef4978073214ecb3365b9e0da27f802db9c245
yelitobimbo <1225100375.ysc@gmail.com>
2026-09-25 14:53:14 -0600

    Define una suma recursiva de arreglos

--- datos/unidad1/recursividad/Recursividad.java
+++ datos/unidad1/recursividad/Recursividad.java
@@ -1,32 +1,40 @@
-package  datos.unidad1.recursividad;
+package datos.unidad1.recursividad;
+
 public class Recursividad {
 
     public static void saludo(int total, String nombre) {
-        if (total <= 0) //Condicion base
-  	return;
-           else{ //Condicion recursiva o repetitiva
-	    System.out.println("Hola  " + nombre);
+        if (total <= 0) {
+            return;
+        } else {
+            System.out.println("Hola  " + nombre);
             saludo(total - 1, nombre);
         }
     }
-/**
-*Funcion que realiza cuenta regresiva de acuerdo a un
-*valor entero
-*Fecha: 25 Septiembre
-*Autor: Yaneli Sanchez Cardenas
-*/
 
-	public static void cuentaRegresiva(int n) {
-	if(n < 0) {
-		return;
-	}else{
-		System.out.println(n);
-		cuentaRegresiva(n-1);
-	}
-}
+    public static void cuentaRegresiva(int numero) {
+        if (numero <= 0) {
+            return;
+        } else {
+            System.out.println(numero);
+            cuentaRegresiva(numero - 1);
+        }
+    }
 
-    public static void main(String[ ] args) {
-        //saludo(10, "Yaneli" , 100);
-    	cuentaRegresiva(100);
-	}
+    public static int sumaRecursiva(int[] datos, int tam) {
+        if (tam <= 0) {
+            return 0;
+        } else {
+            return datos[tam - 1] + sumaRecursiva(datos, tam - 1);
+        }
+    }
+
+    /**
+     * Funcion que realiza suma recursiva de un arreglo
+     * Fecha: 25 Septiembre
+     * Autor: Yaneli Sanchez Cardenas
+     */
+    public static void main(String[] args) {
+        int[] data = {3, 4, 5, 6, 7, 8};
+        System.out.println(sumaRecursiva(data, data.length));
+    }
 }
